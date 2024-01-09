6369e522e2264c3f1f962f28f5e76d05e358f9cd
C Crognier <christophe.crognier@gmail.com>
2024-01-09 19:07:48 +0100

    Basic Router

--- edumation-front/src/App.tsx
+++ edumation-front/src/App.tsx
@@ -1,12 +1,23 @@
 import './index.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Dashboard from './components/Dashboard';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/logout" element={<Logout />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
