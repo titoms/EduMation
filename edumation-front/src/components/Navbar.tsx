6c888cc3ee03aa28bfdd7acd622e034e56a7c5f1
C Crognier <christophe.crognier@gmail.com>
2024-01-11 16:32:45 +0100

    Login/Logout logic on Navbar

--- edumation-front/src/components/Navbar.tsx
+++ edumation-front/src/components/Navbar.tsx
@@ -1,25 +1,40 @@
 import logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  // Check if the user is logged in by verifying the presence of the token
+  const isLoggedIn = !!localStorage.getItem('token');
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Clear the token from localStorage and any other state or context
+    localStorage.removeItem('token');
+    // Redirect or update state as necessary
+    navigate('/');
+  };
+
   return (
     <nav className="bg-navy text-white flex justify-between p-2 center">
       <Link to="/">
         <img src={logo} className="h-10" alt="Logo" />
       </Link>
       <div className="flex align-middle justify-center">
-        <Link
-          to="login"
-          className="text-white m-2 no-underline transition-colors hover:text-terciary"
-        >
-          Login
-        </Link>
-        <Link
-          to="/logout"
-          className="text-white m-2 no-underline transition-colors hover:text-terciary"
-        >
-          Logout
-        </Link>
+        {isLoggedIn ? (
+          <button
+            onClick={handleLogout}
+            className="text-white m-2 no-underline transition-colors hover:text-terciary"
+          >
+            Logout
+          </button>
+        ) : (
+          <Link
+            to="/login"
+            className="text-white m-2 no-underline transition-colors hover:text-terciary"
+          >
+            Login
+          </Link>
+        )}
         <Link
           to="/dashboard"
           className="text-white m-2 no-underline transition-colors hover:text-terciary"
