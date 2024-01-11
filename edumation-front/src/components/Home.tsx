7a9976ebcbb0dadcae16096e3facb49bedb009b4
C Crognier <christophe.crognier@gmail.com>
2024-01-11 16:39:22 +0100

    Login page upgrade and Home small changes

--- edumation-front/src/components/Home.tsx
+++ edumation-front/src/components/Home.tsx
@@ -9,8 +9,8 @@ function Home() {
           <h1 className="text-white font-extrabold text-4xl xs:text-5xl md:text-6xl">
             Right Place To
           </h1>
-          <h2 className="text-white font-extrabold text-3xl xs:text-4xl md:text-5xl leading-tight">
-            Get a Better and Professionals Design
+          <h2 className="text-white font-extrabold text-2xl xs:text-4xl md:text-5xl leading-tight">
+            Manage time and learning for Schools, Teachers & Students
           </h2>
           <div className="inline-flex">
             <button className="p-2 my-5 mx-2 bg-indigo-700 hover:bg-indigo-800 font-bold text-white rounded border-2 border-transparent hover:border-indigo-800 shadow-md transition duration-500 md:text-xl">
--- edumation-front/src/components/Login.tsx
+++ edumation-front/src/components/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import logo from '../assets/logo.png';
 
 const Login = () => {
   const [email, setEmail] = useState<string>('');
@@ -36,50 +37,55 @@ const Login = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="p-6 max-w-sm w-full bg-white shadow-md rounded-md">
-        <h2 className="text-xl font-semibold text-gray-700 text-center">
-          Login
-        </h2>
-        {errorMessage && (
-          <p className="text-red-500 text-sm text-center">{errorMessage}</p>
-        )}
-        <form onSubmit={handleLogin} className="mt-4">
-          <div>
-            <label className="block" htmlFor="email">
-              Email:
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
+    <>
+      <div className="items-center h-screen bg-gray-100 p-6">
+        <img src={logo} className="h-40 m-auto" alt="Logo" />
+        <div className="flex justify-center mt-8">
+          <div className="p-6 max-w-sm w-full bg-white shadow-md rounded-md">
+            <h2 className="text-xl font-semibold text-gray-700 text-center">
+              Login
+            </h2>
+            {errorMessage && (
+              <p className="text-red-500 text-sm text-center">{errorMessage}</p>
+            )}
+            <form onSubmit={handleLogin} className="mt-4">
+              <div>
+                <label className="block" htmlFor="email">
+                  Email:
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="mt-4">
+                <label className="block" htmlFor="password">
+                  Password:
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full px-4 py-2 mt-6 font-medium text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
+              >
+                Login
+              </button>
+            </form>
           </div>
-          <div className="mt-4">
-            <label className="block" htmlFor="password">
-              Password:
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full px-4 py-2 mt-6 font-medium text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
-          >
-            Login
-          </button>
-        </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
