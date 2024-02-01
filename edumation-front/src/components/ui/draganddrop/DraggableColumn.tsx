b7ece3709def44705a1228cd65c6ae8d77871dbd
C Crognier <christophe.crognier@gmail.com>
2024-02-01 14:37:46 +0100

    DraggableColumn now has an incorporated searchbar

--- edumation-front/src/components/ui/draganddrop/DraggableColumn.tsx
+++ edumation-front/src/components/ui/draganddrop/DraggableColumn.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Droppable } from 'react-beautiful-dnd';
 import { styled } from '@stitches/react';
 import { DraggableItem } from './DraggableItem';
@@ -32,16 +32,42 @@ const StyledList = styled('div', {
   marginTop: 8,
 });
 
+const StyledSearch = styled('input', {
+  marginBottom: '4px',
+  backgroundColor: '#ddd',
+  padding: '4px',
+  borderRadius: '4px',
+  border: '1px solid #ccc',
+  width: '100%',
+  boxSizing: 'border-box',
+});
+
 export const DraggableColumn: React.FC<ColumnProps> = ({
   col: { list, id },
 }) => {
+  const [searchFilter, setSearchFilter] = useState('');
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchFilter(event.target.value);
+  };
+
+  const filteredList = list.filter((student) =>
+    student.name.toLowerCase().includes(searchFilter.toLowerCase())
+  );
+
   return (
     <Droppable droppableId={id}>
       {(provided) => (
         <StyledColumn>
           <h2>{id}</h2>
+
           <StyledList {...provided.droppableProps} ref={provided.innerRef}>
-            {list.map((student, index) => (
+            <StyledSearch
+              type="text"
+              placeholder="Search students..."
+              onChange={handleSearchChange}
+            />
+            {filteredList.map((student, index) => (
               <DraggableItem
                 key={student._id}
                 student={student}
