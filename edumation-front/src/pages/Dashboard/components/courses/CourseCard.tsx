c4377287b52d3afe1d904f1e53d0e0c62b200bdf
C Crognier <christophe.crognier@gmail.com>
2024-02-09 17:35:20 +0100

    CourseCard displays teacher picture and name

--- edumation-front/src/pages/Dashboard/components/courses/CourseCard.tsx
+++ edumation-front/src/pages/Dashboard/components/courses/CourseCard.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Course } from '../../../../services/Types';
+import React, { useEffect, useState } from 'react';
+import { Course, User } from '../../../../services/Types';
 import { Button } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { toast } from 'react-toastify';
 import { Link } from 'react-router-dom';
+import UsersService from '../../../../services/UsersService';
 
 interface CourseCardProps {
   course: Course;
@@ -11,6 +12,21 @@ interface CourseCardProps {
 }
 
 const CourseCard: React.FC<CourseCardProps> = ({ course, onDelete }) => {
+  const [teacher, setTeacher] = useState<User | null>(null);
+
+  useEffect(() => {
+    if (course.teacherId) {
+      UsersService.getUserById(course.teacherId)
+        .then((response) => {
+          setTeacher(response.data);
+        })
+        .catch((error) => {
+          console.error('Failed to fetch teacher details', error);
+          toast.error('Failed to fetch teacher details');
+        });
+    }
+  }, [course.teacherId]);
+
   const handleDelete = () => {
     if (course._id) {
       onDelete(course._id);
@@ -20,38 +36,59 @@ const CourseCard: React.FC<CourseCardProps> = ({ course, onDelete }) => {
   };
 
   return (
-    <div className="bg-white shadow rounded-lg p-4">
-      <Link to={course._id}>
-        <h3 className="text-lg font-semibold">{course.title}</h3>
-      </Link>
-      <div className="my-4">
-        {course.schoolId && <p>School ID: {course.schoolId}</p>}
-        {course.teacherId && <p>Teacher ID: {course.teacherId}</p>}
-        {course.quizIds && course.quizIds.length > 0 && (
-          <p>Quiz IDs: {course.quizIds.join(', ')}</p>
-        )}
-        <p>
-          Duration:{' '}
-          {course.courseDuration
-            ? `${course.courseDuration} days`
-            : 'Not specified'}
-        </p>
-      </div>
+    <>
+      <div className="bg-white shadow rounded-lg p-4">
+        <Link to={`${course._id}`}>
+          <h3 className="text-lg text-center font-semibold">{course.title}</h3>
 
-      <hr />
-      <p className="mt-4">{course.description}</p>
-      <div className="flex justify-end gap-2 mt-4">
-        <Button
-          size="small"
-          variant="outlined"
-          color="error"
-          startIcon={<DeleteIcon />}
-          onClick={handleDelete}
-        >
-          <span className="hidden md:inline">Delete</span>
-        </Button>
+          <div className="my-4">
+            {course.teacherId && (
+              <>
+                <div className="flex justify-around items-center">
+                  <p>Teacher:</p>
+                  <div className="flex justify-evenly my-8 flex-col items-center text-center">
+                    <img
+                      className="w-8 h-8 rounded-full"
+                      src={
+                        teacher?.profileImage ||
+                        'https://via.placeholder.com/150'
+                      }
+                      alt="Teacher Pic"
+                    />
+                    <span>{teacher?.name}</span>
+                  </div>
+                </div>
+              </>
+            )}
+
+            {course.quizIds && course.quizIds.length > 0 && (
+              <p>Quiz IDs: {course.quizIds.join(', ')}</p>
+            )}
+            <p>
+              Duration:{' '}
+              {course.courseDuration
+                ? `${course.courseDuration} days`
+                : 'Not specified'}
+            </p>
+          </div>
+
+          <hr />
+          <p className="mt-4">{course.description}</p>
+        </Link>
+
+        <div className="flex justify-end gap-2 mt-4">
+          <Button
+            size="small"
+            variant="outlined"
+            color="error"
+            startIcon={<DeleteIcon />}
+            onClick={handleDelete}
+          >
+            <span className="hidden md:inline">Delete</span>
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
