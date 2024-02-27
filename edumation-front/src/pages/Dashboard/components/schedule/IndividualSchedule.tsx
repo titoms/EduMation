9feb718918afaf215a2db676c97f55b91f11187f
C Crognier <christophe.crognier@gmail.com>
2024-02-27 21:41:55 +0100

    Small refactor of individualSchedule

--- edumation-front/src/pages/Dashboard/components/schedule/IndividualSchedule.tsx
+++ edumation-front/src/pages/Dashboard/components/schedule/IndividualSchedule.tsx
@@ -5,10 +5,8 @@ import ScheduleSkeleton from '../../../../components/ui/skeletons/ScheduleSkelet
 import { MyEvent, Schedule } from '../../../../services/Types';
 import SchedulesService from '../../../../services/SchedulesService';
 import BackButton from '../../../../components/ui/BackButton';
-import { Calendar, dayjsLocalizer, SlotInfo } from 'react-big-calendar';
-import withDragAndDrop, {
-  EventInteractionArgs,
-} from 'react-big-calendar/lib/addons/dragAndDrop';
+import { Calendar, dayjsLocalizer } from 'react-big-calendar';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import dayjs from 'dayjs';
@@ -19,111 +17,58 @@ import EditEventModal from './EditEventModal';
 const DnDCalendar = withDragAndDrop(Calendar);
 
 const IndividualSchedule = () => {
-  const params = useParams<{ id: string }>();
-  const scheduleId = params.id || '';
+  const { id: scheduleId } = useParams<{ id: string }>();
   const { mode } = useThemeContext();
   const [schedule, setSchedule] = useState<Schedule | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const localizer = dayjsLocalizer(dayjs);
   const [events, setEvents] = useState<MyEvent[]>([]);
   const [editingEvent, setEditingEvent] = useState<MyEvent | null>(null);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [loading, setLoading] = useState<boolean>(true);
+  const localizer = dayjsLocalizer(dayjs);
 
   useEffect(() => {
-    const fetchSchedule = async () => {
-      if (!scheduleId) {
-        toast.error('Schedule ID is undefined');
-        setLoading(false);
-        return;
-      }
-
+    const fetchAndUpdateSchedule = async () => {
+      setLoading(true);
       try {
-        const scheduleResponse = await SchedulesService.getScheduleById(
-          scheduleId
-        );
-        setSchedule(scheduleResponse.data);
-        const fetchedEvents =
-          scheduleResponse.data.events?.map((event) => ({
+        const { data } = await SchedulesService.getScheduleById(scheduleId);
+        setSchedule(data);
+        const mappedEvents =
+          data.events?.map((event) => ({
             start: new Date(event.start),
             end: new Date(event.end),
             title: event.title,
             location: event.location,
           })) || [];
-        setEvents(fetchedEvents);
+        setEvents(mappedEvents);
       } catch (error) {
         toast.error('Failed to fetch schedule details');
       } finally {
         setLoading(false);
       }
     };
-    fetchSchedule();
+
+    if (scheduleId) fetchAndUpdateSchedule();
   }, [scheduleId]);
 
-  const handleSelectSlot = async ({ start, end }: MyEvent) => {
-    const title = window.prompt('New Event title');
-    if (title) {
-      setEvents((prevEvents) => [...prevEvents, { start, end, title }]);
-    }
-  };
-
-  const handleUpdateEvent = async ({ event, start, end }: MyEvent) => {
-    const updatedEvent = { ...event, start, end };
-    setEvents((prevEvents) =>
-      prevEvents.map((evt) => (evt === event ? updatedEvent : evt))
+  const handleEventChange = async (
+    updatedEvent: MyEvent,
+    originalEvent: MyEvent
+  ) => {
+    const updatedEvents = events.map((event) =>
+      event === originalEvent ? { ...updatedEvent } : event
     );
-  };
-
-  const handleEventResize = async ({ event, start, end }: MyEvent) => {
-    const updatedEvent = { ...event, start, end };
-    setEvents((prevEvents) =>
-      prevEvents.map((evt) => (evt === event ? updatedEvent : evt))
-    );
-  };
-
-  const handleDoubleClickEvent = (event: MyEvent) => {
-    setEditingEvent(event);
-    setIsEditModalOpen(true);
-  };
-
-  const handleSubmitEdit = (editedEvent: MyEvent) => {
-    setEvents((prevEvents) =>
-      prevEvents.map((evt) => (evt === editingEvent ? editedEvent : evt))
-    );
-    setIsEditModalOpen(false);
-    updateEventInBackend();
-  };
-
-  const updateEventInBackend = async () => {
+    setEvents(updatedEvents);
     try {
-      await SchedulesService.updateSchedule(scheduleId, { events });
+      await SchedulesService.updateSchedule(scheduleId, {
+        events: updatedEvents,
+      });
+      toast.success('Schedule updated successfully');
     } catch (error) {
       toast.error('Failed to update schedule');
     }
   };
 
-  useEffect(() => {
-    const updateBackend = async () => {
-      if (events.length > 0) {
-        await updateEventInBackend();
-      }
-    };
-    updateBackend();
-  }, [events]);
-
   if (loading) return <ScheduleSkeleton />;
-
-  const componentStyle =
-    mode === 'light'
-      ? {
-          color: '#000',
-          padding: '10px',
-          borderRadius: '5px',
-        }
-      : {
-          color: '#fff',
-          padding: '10px',
-          borderRadius: '5px',
-        };
 
   return (
     <>
@@ -136,15 +81,26 @@ const IndividualSchedule = () => {
           <CalendarActions />
           <DnDCalendar
             className="mt-4"
-            style={componentStyle}
+            style={
+              mode === 'light'
+                ? { color: '#000', padding: '10px', borderRadius: '5px' }
+                : { color: '#fff', padding: '10px', borderRadius: '5px' }
+            }
             localizer={localizer}
             events={events}
             resizable
             selectable
-            onEventDrop={handleUpdateEvent}
-            onEventResize={handleEventResize}
-            onDoubleClickEvent={handleDoubleClickEvent}
-            onSelectSlot={handleSelectSlot}
+            onEventDrop={({ event, start, end }) =>
+              handleEventChange({ ...event, start, end }, event)
+            }
+            onEventResize={({ event, start, end }) =>
+              handleEventChange({ ...event, start, end }, event)
+            }
+            onDoubleClickEvent={setEditingEvent}
+            onSelectSlot={({ start, end }) => {
+              const title = window.prompt('New Event title');
+              if (title) setEvents([...events, { start, end, title }]);
+            }}
             draggableAccessor={() => true}
           />
         </div>
@@ -153,7 +109,10 @@ const IndividualSchedule = () => {
         <EditEventModal
           event={editingEvent}
           onClose={() => setIsEditModalOpen(false)}
-          onSubmit={handleSubmitEdit}
+          onSubmit={(editedEvent) => {
+            handleEventChange(editedEvent, editingEvent);
+            setIsEditModalOpen(false);
+          }}
         />
       )}
     </>
