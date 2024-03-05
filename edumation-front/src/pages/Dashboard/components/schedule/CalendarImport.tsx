3313edf5348d1439a7ae65b9d94227861bd63e16
C Crognier <christophe.crognier@gmail.com>
2024-03-05 19:08:40 +0100

    CalendarImport, check cal-import

---
+++ edumation-front/src/pages/Dashboard/components/schedule/CalendarImport.tsx
@@ -0,0 +1,16 @@
+// CalendarImport.js
+import DragAndDrop from '../../../../components/ui/draganddrop/DragAndDrop';
+
+const CalendarImport = ({ onEventsImported }) => {
+  const handleImportCalendar = async (event) => {};
+
+  const parseIcsFile = (icsData) => {};
+
+  return (
+    <>
+      <DragAndDrop onFileDrop={handleImportCalendar} fileType="csv" />
+    </>
+  );
+};
+
+export default CalendarImport;
