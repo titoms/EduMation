58f9533ff457a4b46c412a458c673606b0964959
C Crognier <christophe.crognier@gmail.com>
2024-03-17 23:08:55 +0100

    Started individualquizz component

--- edumation-front/src/pages/Dashboard/components/quizz/IndividualQuizz.tsx
+++ edumation-front/src/pages/Dashboard/components/quizz/IndividualQuizz.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import BackButton from '../../../../components/ui/BackButton';
 
 const IndividualQuizz = () => {
-  return <div></div>;
+  return (
+    <>
+      <BackButton />
+      <h1 className="text-2xl my-4 font-semibold">Quizz</h1>
+    </>
+  );
 };
 
 export default IndividualQuizz;
