8c163143f7abbcbb71cbf3dfb7251c07a19366ad
C Crognier <christophe.crognier@gmail.com>
2024-03-18 11:18:51 +0100

    Quizz Card updated, delete function not working

--- edumation-front/src/pages/Dashboard/components/quizz/QuizzCard.tsx
+++ edumation-front/src/pages/Dashboard/components/quizz/QuizzCard.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { IconButton, Link } from '@mui/material';
+import { IconButton } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import EditIcon from '@mui/icons-material/Edit';
@@ -17,11 +19,14 @@ const QuizzCard: React.FC<QuizzCardProps> = ({ quiz, onOpenDelete }) => {
       key={quiz._id}
       className="bg-white dark:bg-slate-800 shadow rounded-lg p-4"
     >
-      <Link to={quiz._id}>
+      <RouterLink
+        to={`${quiz._id}`}
+        style={{ textDecoration: 'none', color: 'inherit' }}
+      >
         <h3 className="text-lg font-semibold hover:text-gray-400">
           {quiz.title}
-        </h3>
-      </Link>
+        </h3>{' '}
+      </RouterLink>
       <p>{quiz.description}</p>
       <ul>
         {quiz.questions.map((question, index) => (
@@ -49,14 +54,11 @@ const QuizzCard: React.FC<QuizzCardProps> = ({ quiz, onOpenDelete }) => {
         >
           <ShareIcon sx={{ color: '#3c96c9' }} />
         </IconButton>
-        <Link to={quiz._id}>
-          <IconButton
-            className="text-black dark:text-gray-200"
-            aria-label="share"
-          >
+        <RouterLink to={`${quiz._id}`}>
+          <IconButton aria-label="edit">
             <EditIcon sx={{ color: '#3dc8eb' }} />
           </IconButton>
-        </Link>
+        </RouterLink>
         <IconButton
           className="text-black dark:text-gray-200"
           aria-label="share"
